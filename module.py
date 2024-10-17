b878bdf407bb6a2112548b2570b35955ac2701ef
vincet <vincentbui2108@gmail.com>
2024-10-18 00:01:40 +0300

    first push

--- module.py
+++ module.py
@@ -1,6 +1,8 @@
 import asyncio
 from js import document
 
+
+
 async def stop_watch(*kargs):
     document.getElementById("btn1").innerHTML = 'Get Set'
     document.getElementById("btn1").className = "btn btn-warning"
@@ -19,20 +21,19 @@ async def stop_watch(*kargs):
     hours = 0
 
     while(True):       
-            #print(f'{hours:02} : {minute:02} : {second:02} : {nsecond:02}')     
-            await asyncio.sleep(1/1000)    
-            msecond+=1    
-            if msecond == 99:
-                msecond = 0
-                second +=1             
-            if second == 60:    
-                second = 0    
-                minute+=1    
-            if minute == 60:    
-                minute = 0    
-                hours+=1
-            #print(f'{hours:02} : {minute:02} : {second:02} : {msecond:02}')
-            timestamp = f'{hours:02} : {minute:02} : {second:02} : {msecond:02}'
-            pyscript.write('time', timestamp)
-        
+        #print(f'{hours:02} : {minute:02} : {second:02} : {nsecond:02}')
+        await asyncio.sleep(1/1000)    
+        msecond+=1    
+        if msecond == 99:
+            msecond = 0
+            second +=1             
+        if second == 60:    
+            second = 0    
+            minute+=1    
+        if minute == 60:    
+            minute = 0    
+            hours+=1
+        #print(f'{hours:02} : {minute:02} : {second:02} : {msecond:02}')
+        timestamp = f'{hours:02} : {minute:02} : {second:02} : {msecond:02}'
+        pyscript.write('time', timestamp)
         

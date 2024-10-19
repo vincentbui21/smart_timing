acbc180b2bcf3868fe3af2d62ce4cbbbca1b8b6d
vincet <vincentbui2108@gmail.com>
2024-10-19 04:40:04 +0300

    fixed threading issues, working UI and server

--- server_code.py
+++ server_code.py
@@ -1,39 +1,7 @@
-import socket,sys
+import time
+import streamlit as st
 
-# get the hostname
-host = '212.90.75.100'
-port = 8080  # initiate port no above 1024
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
-# look closely. The bind() function takes tuple as argument
-sockaddr = socket.getaddrinfo(host, port)[0][-1]
-print(sockaddr)
-
-#server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-#server_socket.bind(sockaddr)  # bind host address and port together
-server_socket.bind((host, port))  # bind host address and port together
-
-# configure how many client the server can listen simultaneously
-server_socket.listen(1)
-conn, address = server_socket.accept()  # accept new connection
-print("Connection from: " + str(address))
-
-def player_detected():
-    while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
-            pass
-        else:
-            print("from connected user: " + str(data))
-            return str(data)
-        #data = input(' -> ')
-        #conn.send(data.encode())  # send data to the client
-
-    conn.close()  # close the connection
-
-
-if __name__ == '__main__':
-    player_detected()
-    
+st.info('hi')
+with st.spinner('wait'):
+    time.sleep(5)
+st.success('Done')
--- ui.py
+++ ui.py
@@ -2,13 +2,31 @@ import streamlit as st
 import time
 import asyncio
 import socket,sys
-
+import threading
 
 if 'title' not in st.session_state:
     st.session_state.title = '00:00:00:00'
+if 'conn' not in st.session_state:
+    with st.spinner('Waiting for connection from ESP32...'):
+        host = '212.90.75.100'
+        port = 8080  # initiate port no above 1024
+
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
+        sockaddr = socket.getaddrinfo(host, port)[0][-1]
+        print(sockaddr)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((host, port))  # bind host address and port together
+
+        # configure how many client the server can listen simultaneously
+        server_socket.listen(5)
+        conn, address = server_socket.accept()  # accept new connection
+
+        st.session_state.conn = conn
+    st.success('Socket connection established!')
+
+stop_event = threading.Event()
 
 
-# get the hostname
 def player_detected(conn):
     while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
@@ -17,49 +35,36 @@ def player_detected(conn):
             # if data is not received break
             break
         elif str(data) == 'True':
-            return 'True'
+            stop_event.set()
         elif str(data)=='False':
-            return False
+            pass
+
 
 def start():
-    host = '212.90.75.100'
-    port = 8080  # initiate port no above 1024
-
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
-    sockaddr = socket.getaddrinfo(host, port)[0][-1]
-    print(sockaddr)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(5)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
-    st.write("Connection from: " + str(address))
-
     for i in range (1,4):
         st.session_state.title = 4-i
         title_placeholder.title(st.session_state.title)
         time.sleep(1)
     
+    myThread = threading.Thread(target=player_detected, daemon=True, args=(st.session_state.conn,))
+    myThread.start()
+
     msecond = 0
     second = 0    
     minute = 0    
     hour = 0
-    while(True):       
-        time.sleep(1/1000)        
+    while not stop_event.is_set():       
+        time.sleep(1/100)        
         msecond+=1    
         if msecond == 99:
             msecond = 0
             second +=1             
-        if second == 60:    
+        elif second == 60:    
             second = 0    
             minute+=1    
-        if minute == 60:    
+        elif minute == 60:    
             minute = 0    
             hour+=1
-        if player_detected(conn) == 'True':
-            break
         title_placeholder.title(f'{hour:02} : {minute:02} : {second:02} : {msecond:02}')
         
 
